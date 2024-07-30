a7e69dd6492acb97a9814549b73e57583e2f8ef3
jtitor <1165137798@qq.com>
2024-07-30 18:28:18 +0800

    first commit

--- train_state.py
+++ train_state.py
@@ -132,7 +132,7 @@ def create_train_state(rng,
                                     trade_beta=trade_beta, label_smoothing=label_smoothing)
 
     abstract_variables = jax.eval_shape(
-        functools.partial(init_fn, model=model, ), input_data)
+        functools.partial(init_fn, model=model, optimizer=tx), input_data)
 
     state_sharding = nn.get_sharding(abstract_variables, mesh)
 
