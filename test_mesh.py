8e623723c1e060ddd1bcfaf3c9c65e0057e18f8a
jtitor <1165137798@qq.com>
2024-07-30 15:16:49 +0800

    first commit

--- test_mesh.py
+++ test_mesh.py
@@ -5,6 +5,18 @@ import flax.linen as nn
 from jax.sharding import Mesh,PartitionSpec,NamedSharding
 from jax.experimental import mesh_utils
 
+
+Device=jax.Device
+
+
+def get_device_coords_tpu(device:Device):
+    assert device.platform=='tpu'
+    print(device.core_on_chip)
+
+
+def get_hardware_mesh_tpu(devices):
+    # mesh_dict=
+    pass
 def go():
 
     device_mesh=mesh_utils.create_device_mesh((jax.device_count(),))
@@ -12,6 +24,8 @@ def go():
 
     if jax.process_index()==0:
         print(mesh)
+
+        get_device_coords_tpu()
 
 
     pass
