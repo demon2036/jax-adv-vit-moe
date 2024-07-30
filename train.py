2e6095828862f18434305f4bc0b95fc02884f34d
jtitor <1165137798@qq.com>
2024-07-30 19:39:12 +0800

    first commit

--- train.py
+++ train.py
@@ -29,6 +29,42 @@ def train_step(x, state: EMATrainState):
     return state
 
 
+# if jax.process_index() == 0:
+#     print(device_mesh)
+#     # print(x_sharding.addressable_devices)
+#     # print()
+#     # print(mesh)
+#     # jax.debug.visualize_sharding((shape[0], shape[1]), sharding=x_sharding)
+#     # jax.debug.visualize_array_sharding(global_batch_array[:, :, 0])
+#     #
+#     # print(x_sharding.addressable_devices)
+#     # print(state_sharding)
+#     # jax.debug.visualize_array_sharding(grad['Dense_0']['kernel'])
+#     # print(params)
+#     print(global_batch_array.shape)
+#     print(end - start)
+
+# print(grad)
+
+
+def convert_to_global_array(x, x_sharding):
+    b, *res = x.shape
+
+    per_replica_batches_x = np.split(x, jax.local_device_count())
+
+    global_batch_shape_x = (b * jax.process_count(), *res)
+
+    global_batch_array_x = jax.make_array_from_single_device_arrays(
+        global_batch_shape_x, sharding=x_sharding,
+        arrays=[
+            jax.device_put(batch, device)
+            for batch, device in zip(per_replica_batches_x, x_sharding.addressable_devices)
+        ]
+    )
+
+    return global_batch_array_x
+
+
 def train():
     device_mesh = mesh_utils.create_device_mesh((jax.device_count(),))
     mesh = Mesh(device_mesh, axis_names=('data',))
@@ -38,7 +74,6 @@ def train():
 
     x_sharding = mesh_sharding(PartitionSpec('data'))
     rng = jax.random.PRNGKey(1)
-    state, state_sharding = create_train_state(rng, x_sharding, mesh, dim=768)
 
     # shape = (128, 256, 384)
     shape = (128, 3, 32, 32,)
@@ -72,6 +107,14 @@ def train():
         ]
     )
 
+    global_batch_array_x = convert_to_global_array(x, x_sharding)
+    print(global_batch_array_x.shape)
+
+    while True:
+        pass
+
+    state, state_sharding = create_train_state(rng, x_sharding, mesh, dim=768)
+
     # train_step_jit = jax.jit(train_step, in_shardings=(x_sharding, state_sharding), out_shardings=state_sharding, )
 
     train_step_jit = jax.jit(apply_model_trade,
@@ -96,23 +139,6 @@ def train():
                 state = block_all(train_step_jit(state, (global_batch_array_x, global_batch_array_y), rng))
 
                 pbar.update()
-
-        # if jax.process_index() == 0:
-        #     print(device_mesh)
-        #     # print(x_sharding.addressable_devices)
-        #     # print()
-        #     # print(mesh)
-        #     # jax.debug.visualize_sharding((shape[0], shape[1]), sharding=x_sharding)
-        #     # jax.debug.visualize_array_sharding(global_batch_array[:, :, 0])
-        #     #
-        #     # print(x_sharding.addressable_devices)
-        #     # print(state_sharding)
-        #     # jax.debug.visualize_array_sharding(grad['Dense_0']['kernel'])
-        #     # print(params)
-        #     print(global_batch_array.shape)
-        #     print(end - start)
-
-        # print(grad)
 
     return state
 
