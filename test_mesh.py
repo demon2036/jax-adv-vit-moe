da1ba40a6cb33bbc6030f226405548c3ecf6ffba
jtitor <1165137798@qq.com>
2024-07-30 15:20:11 +0800

    first commit

--- test_mesh.py
+++ test_mesh.py
@@ -13,6 +13,9 @@ def get_device_coords_tpu(device:Device):
     assert device.platform=='tpu'
     print(device)
     print(device.core_on_chip)
+    print(device.default_memory())
+    print(device.memory_stats())
+    print(device.coords)
 
 
 def get_hardware_mesh_tpu(devices):
