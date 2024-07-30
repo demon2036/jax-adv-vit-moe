7837e9db312e130ea95ab7db10d8a7ecb1f1d5b8
jtitor <1165137798@qq.com>
2024-07-30 15:24:33 +0800

    first commit

--- test_mesh.py
+++ test_mesh.py
@@ -2,43 +2,48 @@ import jax
 import jax.numpy as jnp
 import flax
 import flax.linen as nn
-from jax.sharding import Mesh,PartitionSpec,NamedSharding
+from jax.sharding import Mesh, PartitionSpec, NamedSharding
 from jax.experimental import mesh_utils
 
+Device = jax.Device
 
-Device=jax.Device
 
-
-def get_device_coords_tpu(device:Device):
-    assert device.platform=='tpu'
+def get_device_coords_tpu(device: Device):
+    assert device.platform == 'tpu'
     print(device)
     print(device.core_on_chip)
     print(device.default_memory())
     print(device.memory_stats())
     print(device.coords)
+    print()
+    core_on_chip = int(device.core_on_chip)
+    coords = tuple(map(int, device.coords))
+    return core_on_chip, *coords
 
 
 def get_hardware_mesh_tpu(devices):
+
+
+    mesh_dict={get_device_coords_tpu(device):device  for device in devices }
+    print(mesh_dict)
+
     # mesh_dict=
     pass
+
+
 def go():
+    device_mesh = mesh_utils.create_device_mesh((jax.device_count(),))
+    mesh = Mesh(device_mesh, ("data",))
 
-    device_mesh=mesh_utils.create_device_mesh((jax.device_count(),))
-    mesh=Mesh(device_mesh,("data",))
-
-    if jax.process_index()==0:
+    if jax.process_index() == 0:
         print(mesh)
 
         get_device_coords_tpu(jax.devices()[0])
 
-
     pass
 
 
-
-if __name__=="__main__":
-
+if __name__ == "__main__":
     jax.distributed.initialize()
-
 
     go()
