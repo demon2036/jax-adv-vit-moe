894cbce00a7f0ae7a2478ee619d793326ae197ee
jtitor <1165137798@qq.com>
2024-07-30 18:06:08 +0800

    first commit

--- train_state.py
+++ train_state.py
@@ -79,8 +79,8 @@ def create_train_state(rng,
     )
 
     image_shape = [jax.device_count(), 32, 32, 3]
-    if jax.process_index() == 0:
-        print(model.tabulate(rng, jnp.ones(image_shape)))
+    # if jax.process_index() == 0:
+    #     print(model.tabulate(rng, jnp.ones(image_shape)))
 
     input_data = jnp.ones(image_shape)
 
@@ -141,6 +141,7 @@ def create_train_state(rng,
 
     state = jit_init_fn(input_data, model)
     print(state)
+    print(1)
 
     while True:
         pass
