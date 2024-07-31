5c35562501f9c367dc9dabaaa191e68da052e862
jtitor <1165137798@qq.com>
2024-07-31 12:24:37 +0800

    first commit

--- models/model_moe.py
+++ models/model_moe.py
@@ -354,10 +354,10 @@ class ViTLayer(ViTBase, nn.Module):
             # x = x + norm(x)
 
             w = self.param(f'w_{i}', nn.with_partitioning(self.expert_init, ('data',)),
-                           (256, dim, 4 * dim))
+                           (self.num_experts, dim, 4 * dim))
 
             w2 = self.param(f'w2_{i}', nn.with_partitioning(self.expert_init, ('data',)),
-                            (256, 4 * dim, dim))
+                            (self.num_experts, 4 * dim, dim))
 
             # x = with_sharding_constraint(x, mesh_sharding(PartitionSpec('model')))
             # x=jax.lax.all
