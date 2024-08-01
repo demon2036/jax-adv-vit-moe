24ba9543562bfa682442000577010a4a4a7ce90f
jtitor <1165137798@qq.com>
2024-08-01 09:24:51 +0800

    first commit

--- configs/vit-s2-32-cifar10-10000ep-ls0.4-gap-beta3-mp.sh
+++ configs/vit-s2-32-cifar10-10000ep-ls0.4-gap-beta3-mp.sh
@@ -1,5 +1,5 @@
 export EPOCH=2000 TRAIN_BATCH_SIZE=1024 WARMUP_EPOCH=5
-export LIBTPU_INT_ARGS="--xla_enable_async_all_gather=true TPU_MEGACORE=MEGACORE_DENSE"
+export LIBTPU_INIT_ARGS="--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
 
 
 python -u train.py \
