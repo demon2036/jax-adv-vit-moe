3fa3a4402bca1340af2169fe2e2afdf1c1f1cc0c
jtitor <1165137798@qq.com>
2024-08-01 11:34:14 +0800

    first commit

---
+++ configs/vit-s2-32-cifar10-40000ep-ls0.4-gap-beta3-mp.sh
@@ -0,0 +1,33 @@
+export EPOCH=40000 TRAIN_BATCH_SIZE=4096 WARMUP_EPOCH=200
+export LIBTPU_INIT_ARGS="--xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
+
+
+python -u train.py \
+    --train-dataset-shards "$GCS_DATASET_DIR/cifar10-50m-wds/shards-{00000..00999}.tar" \
+    --valid-dataset-shards  "$GCS_DATASET_DIR/cifar10-test-wds/shards-{00000..00099}.tar" \
+    --train-origin-dataset-shards "$GCS_DATASET_DIR/cifar10-train-wds/shards-{00000..00099}.tar" \
+    --layers 12  \
+    --dim 384  \
+    --heads 6  \
+    --labels 10  \
+    --layerscale   \
+    --patch-size 2  \
+    --image-size 32  \
+    --posemb "learnable"  \
+    --pooling gap  \
+    --dropout 0.0  \
+    --droppath 0.0  \
+    --train-batch-size $TRAIN_BATCH_SIZE \
+    --learning-rate 3e-4 \
+    --weight-decay 0.05 \
+    --adam-b1 0.9 \
+    --adam-b2 0.99 \
+    --clip-grad 10.0 \
+    --warmup-steps $((50000 * $WARMUP_EPOCH / $TRAIN_BATCH_SIZE)) \
+    --training-steps $((50000 * $EPOCH / $TRAIN_BATCH_SIZE)) \
+    --eval-interval $((50000 * 50 / $TRAIN_BATCH_SIZE)) \
+    --project cifar1000-ablation-beta-new \
+    --name $(basename $0 .sh) \
+    --output-dir "$GCS_DATASET_DIR/ablation/mp" \
+    --beta 3.0 \
+    --label-smoothing 0.4
